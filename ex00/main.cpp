5d9007dfbf7cf9fd5cafea6cbe57286d7a25715d
John Adithya <jadithya@42student.abudhabi.ae>
2024-04-18 13:49:11 +0400

    finished cpp07

---
+++ ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 13:47:17 by jadithya          #+#    #+#             */
+/*   Updated: 2024/04/18 13:47:49 by jadithya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,17 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Templates.hpp                                      :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 21:27:53 by jadithya          #+#    #+#             */
-/*   Updated: 2024/03/09 21:36:55 by jadithya         ###   ########.fr       */
+/*   Updated: 2024/04/18 13:48:31 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEMPLATES_HPP
 # define TEMPLATES_HPP
+
+# include <iostream>
 
 template <typename T> void swap(T &x, T &y) {
 	T t;
@@ -20,11 +22,11 @@ template <typename T> void swap(T &x, T &y) {
 	y = t;
 }
 
-template <typename T> T min(T x, T y) {
+template <typename T> T min(const T &x, const T &y) {
 	return (x < y ? x : y);
 }
 
-template <typename T> T max(T x, T y) {
+template <typename T> T max(const T &x, const T &y) {
 	return (x > y ? x : y);
 }
 
